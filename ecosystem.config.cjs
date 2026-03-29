912c742ea205bf5d00517382a980a6b3abfe26e7
Jurjen <jgw.waanders@gmail.com>
2026-03-29 19:07:32 +0200

    :sparkles: Fix issue in bot

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,9 +5,12 @@ module.exports = {
       script: 'index.js',
       cwd: __dirname,
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '200M',
+      restart_delay: 5000,
+      max_restarts: 10,
       env: {
         NODE_ENV: 'production',
       },
